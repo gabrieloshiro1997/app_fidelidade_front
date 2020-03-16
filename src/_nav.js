dbe4d089faeeda596a8b34913d8e67b274784f7c
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2020-03-16 12:57:18 -0300

    Modulo usuario

---
+++ src/_nav.js
@@ -0,0 +1,23 @@
+export default {
+  items: [
+    {
+      title: true,
+      name: 'Menu',
+    },
+    {
+      name: 'Home',
+      url: '/',
+      icon: 'icon-home'
+    },
+    {
+      name: 'Usuários',
+      url: '/Usuario',
+      icon: 'icon-user'
+    },
+    {
+      name: 'Cards',
+      url: '/base/cards',
+      icon: 'icon-puzzle',
+    }
+  ],
+};
